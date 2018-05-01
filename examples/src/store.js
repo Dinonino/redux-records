59ed068352ba72da53ef71c7a2e1fe506686c041
Ivan Varga <ivanvarga@ivarga.local>
2018-05-01 23:33:03 +0200

    examples

--- examples/src/store.js
+++ examples/src/store.js
@@ -1,5 +1,7 @@
-import { createStore } from 'redux';
-import { reducersFactory } from '../../src/reducer';
-const reducers = reducersFactory({ USERS: "id" });
-const store = createStore(reducers);
+import { createStore, combineReducers } from 'redux';
+import { reducerFactory, ACTION_CONSTANTS } from 'redux-records';
+const dataReducer = reducerFactory();
+const store = createStore(
+    combineReducers({ [ACTION_CONSTANTS.CONSTANTS_REGEX.ID]: dataReducer }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
