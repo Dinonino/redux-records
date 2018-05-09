07ca888e01a2e5a9e3e1805cf1a1ca9650a5f79e
Ivan Varga <ivanvarga@ivarga.local>
2018-05-10 00:21:28 +0200

    rest api

--- examples/src/store.js
+++ examples/src/store.js
@@ -1,13 +1,58 @@
-import { createStore, combineReducers } from 'redux';
-import { reducerFactory, REDUX_PATHS } from 'redux-records';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { reducerFactory, REDUX_PATHS, ApiMiddleware } from 'redux-records';
 import { reducer as formReducer } from 'redux-form';
 
 const dataReducer = reducerFactory();
+const entityPoint = entity => ({
+  delete: ({ id }) => fetch(`http://localhost:3004/${entity}/${id}`, {
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'DELETE',
+  }),
+  update: ({ id, ...entityBody }) => {
+    if (id) {
+      return fetch(`http://localhost:3004/${entity}/id`, {
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(entityBody),
+        method: 'PUT',
+      });
+    }
+    return fetch(`http://localhost:3004/${entity}`, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(entityBody),
+    }).then(response => response.json().id);
+  },
+  load: (params) => {
+    const query = Object.keys(params)
+      .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
+      .join('&');
+    return fetch(`http://localhost:3004/${entity}?${query}`, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'GET',
+    });
+  },
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(ApiMiddleware({
+  USERS: entityPoint('USERS'),
+  POSTS: entityPoint('POSTS'),
+  COMMENTS: entityPoint('COMMENTS'),
+})));
+
 const store = createStore(
   combineReducers({
     [REDUX_PATHS.STORE_KEY]: dataReducer,
     form: formReducer,
   }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  enhancer,
 );
 export default store;
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,11 +1,10 @@
-import { STORE_KEY, STORE_PATH } from '../reducer/constants';
+import { ACTION } from '../reducer/constants';
 import * as constants from '../actions/constants';
 import actionsFactory from '../actions';
 import { actionsSelector } from '../selectors';
-import { ACTION } from '../reducer/constants';
 
 const isPromise = obj => !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
-const handleAction = (options, entityKey, dispatch) => (payload, apiKey, actionSucceeded, actionFailed) => {
+const handleAction = (options, dispatch) => (payload, apiKey, actionSucceeded, actionFailed) => {
   if (options && apiKey && options[apiKey] && typeof options[apiKey] === 'function') {
     try {
       const resultPromise = options[apiKey](payload);
@@ -26,46 +25,39 @@ const handleAction = (options, entityKey, dispatch) => (payload, apiKey, actionS
   }
 };
 
-let keys = null;
+
 const handleUpdateSuccess = (previousId, updateSucceededAction) => payload => updateSucceededAction(previousId, updateSucceededAction);
 
 
 const apiMiddleware = options => ({ dispatch, getState }) => next => (action) => {
   const { type, payload } = action;
   if (type.startsWith(constants.ID)) {
-    if (!keys) {
-      for (const [key, value] of Object.entries(getState())) {
-        if (value[STORE_PATH.KEY] === STORE_KEY) {
-          keys = object.entries(value).reduce((acc, [dataId, value]) => {
-            acc[dataId] = value[STORE_PATH.KEY];
-            return acc;
-          }, {});
-          break;
-        }
-      }
-    }
     const id = constants.DATA_ID_EXP.exec(type)[0];
-    const actions = actionsFactory(id);
+    const actionCreators = actionsFactory(id);
     const api = options[id];
-    const handler = handleAction(api, keys[api], dispatch);
-    if (constants.DELETE.test(type)) {
-      handler('delete', actions.deleteSucceededAction, actions.deleteFailedAction, payload);
-    } else if (constants.LOAD.test(type)) {
-      handler('load', actions.loadSucceededAction, actions.loadFailedAction, payload);
-    } else if (constants.UPDATE.test(type)) {
-      handler('update', actions.updateSucceededAction, actions.updateFailedAction, payload);
+    const handler = handleAction(api, dispatch);
+    if (constants.DELETE_SYNC.test(type)) {
+      handler('delete', actionCreators.deleteSucceededAction, actionCreators.deleteFailedAction, payload);
+    } else if (constants.LOAD_SYNC.test(type)) {
+      handler('load', actionCreators.loadSucceededAction, actionCreators.loadFailedAction, payload);
+    } else if (constants.LOAD_SYNC.test(type)) {
+      handler('update', actionCreators.updateSucceededAction, actionCreators.updateFailedAction, payload);
     } else if (constants.SYNC_ALL.test(type)) {
       const actions = actionsSelector(getState(), id);
-      for (const action of actions) {
-        const { type, payload } = action;
-        switch (type) {
+
+      for (let index = 0; index < actions.length; index += 1) {
+        const entityAction = actions[index];
+
+
+        const { type: actionType, action: actionPayload } = entityAction;
+        switch (actionType) {
           case ACTION.DELETE:
-            handler('delete', actions.deleteSucceededAction, actions.deleteFailedAction, payload);
+            handler('delete', actions.deleteSucceededAction, actions.deleteFailedAction, actionPayload);
             break;
           case ACTION.UPDATE:
             handler('update', (result) => {
 
-            }, handleUpdateSuccess(id, actions.updateSucceededAction), actions.updateFailedAction, payload);
+            }, handleUpdateSuccess(id, actions.updateSucceededAction), actions.updateFailedAction, actionPayload);
             break;
           default:
             break;
