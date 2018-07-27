26b4b013e1c232db04e96fcfac2a76b8770d30a2
Ivan Varga <ivanvarga@ivarga.local>
2018-07-27 13:58:11 +0200

    semantic build

--- examples/src/scenes/basic/list.js
+++ examples/src/scenes/basic/list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { compose } from 'redux';
-import { Table, Button, Loader, Container } from 'semantic-ui-react';
+import { Table, Button, Loader, Container, Icon } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
 import dataContainer from '../../components/datacontainer';
 import CodeDoc from '../../components/codeDoc';
@@ -84,12 +84,13 @@ const listScreen = (props) => {
   } = props;
   return (
     <Container>
-      <Loader size='large' active={STATUS === 'SYNCING'} inline='centered'>Loading</Loader>
-      <Table celled>
+      <Table striped stackable>
         <Table.Header>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Username</Table.HeaderCell>
-          <Table.HeaderCell />
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Username</Table.HeaderCell>
+            <Table.HeaderCell>&nbsp;</Table.HeaderCell>
+          </Table.Row>
         </Table.Header>
         <Table.Body>
           {data.map(({
@@ -100,12 +101,12 @@ const listScreen = (props) => {
                 <Table.Cell><Link to={`/redux-records/edit/${id}`}>{username}</Link></Table.Cell>
                 <Table.Cell>
                   <Loader size='mini' active={ENTITIES_STATE[id].STATUS === 'SYNCING'} inline>Deleting</Loader>
-                  <Button size="small" onClick={() => deleteAction({ id })}>Delete</Button>
+                  <Icon name="user delete" size="large" onClick={() => deleteAction({ id })}></Icon>
                 </Table.Cell>
               </Table.Row>))}
         </Table.Body>
       </Table>
-      <Button primary onClick={() => loadAction()}>Load Users</Button>
+      {STATUS === 'SYNCING' ? (<Loader size='large' active={true} inline='centered'>Loading</Loader>) : (<Button primary onClick={() => loadAction()}>Load Users</Button>)}
       <CodeDoc code={code} />
     </Container>
   );
